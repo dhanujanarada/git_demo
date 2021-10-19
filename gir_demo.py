6b36731b110ebc73871b2229af714794aa75d7c2
dhanujanarada <75336972+dhanujanarada@users.noreply.github.com>
2021-10-19 11:10:30 +0530

    commit  one

---
+++ gir_demo.py
@@ -0,0 +1,9 @@
+a=3 
+o=0
+print('you have: ',a,'oranges and',o,'apples')
+buy_a=4 
+buy_o=6 
+print('you buy: ',buy_a,'apples and',buy_o,'oranges')
+a=a+buy_a
+o=o+buy_o
+print('you now have:',a,'apples and',o,'oranges')
